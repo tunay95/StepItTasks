2c2802a3fda28f0c22bfac2aeacd4f6bdb2792d4
tunay95 <cypherhsynl1@gmail.com>
2024-12-01 05:22:17 +0400

    Question, Test, Category, User & Admin classes were structed.

--- CppTasks/Projects/ExamOopProject/ExamOopProject/ExamOopProject.cpp
+++ CppTasks/Projects/ExamOopProject/ExamOopProject/ExamOopProject.cpp
@@ -1,12 +1,26 @@
-#include<iostream>
+﻿#include<iostream>
 #include<memory>
 #include<map>
 #include<cstring>
+#include<string>
 #include<cctype>
 #include <vector>
 #include <cmath>
 
 using namespace std;
+
+
+class Question;
+class TrueFalseQuestion;
+class OpenEndedQuestion;
+class FourOptionQuestion;
+class Test;
+class Category;
+class User;
+class Database;
+class Admin;
+class UserManager;
+class AdminManager;
 
 
 class Question
@@ -15,7 +29,6 @@ protected:
 
 	string statement;
 	string answer;
-	char userAnswer;
 
 public:
 	Question(const string& statement, string answer) :statement(statement), answer(answer)
@@ -23,7 +36,7 @@ public:
 
 	virtual void ShowQuestion() {};
 
-	virtual bool CheckAnswer() = 0;
+	virtual bool CheckAnswer(string userAnswer) = 0;
 
 
 	string GetStatement() const
@@ -47,17 +60,6 @@ public:
 		this->answer = answer;
 	}
 
-
-	char GetUserAnswer() const
-	{
-		return userAnswer;
-	}
-
-	void SetUserAnswer(char userAnswer)
-	{
-		this->userAnswer = userAnswer;
-	}
-
 };
 
 
@@ -73,15 +75,11 @@ public:
 		cout << "Question: " << statement << endl;
 
 		cout << "\nA) True \nB) False\n";
-
-		cout << "\nYour Anwser: ";
-
-		cin >> userAnswer;
 	}
 
-	bool CheckAnswer() override
+	bool CheckAnswer(string userAnswer) override
 	{
-		if (toupper(userAnswer) == 'A')
+		if (toupper(userAnswer[0]) == 'A')
 		{
 			return answer == "True";
 		}
@@ -106,13 +104,9 @@ public:
 	void ShowQuestion() override
 	{
 		cout << "Question: " << statement << endl << endl;
-
-		cout << "\nYour Anwser: ";
-		cin >> userAnswer;
-		userAnswer[0] = toupper(userAnswer[0]);
 	}
 
-	bool CheckAnswer() override
+	bool CheckAnswer(string userAnswer) override
 	{
 		return userAnswer == answer;
 	}
@@ -146,16 +140,11 @@ public:
 			questionOptions2[char(65 + i)] = questionOptions[randIndex];
 			questionOptions.erase(questionOptions.begin() + randIndex);
 		}
-
-		cout << "\nYour Anwser: ";
-
-		cin >> userAnswer;
-		userAnswer = toupper(userAnswer);
 	}
 
-	bool CheckAnswer() override
+	bool CheckAnswer(string userAnswer) override
 	{
-		if (questionOptions2[userAnswer] == answer)
+		if (questionOptions2[userAnswer[0]] == answer)
 		{
 			return true;
 		}
@@ -170,22 +159,27 @@ class Test
 {
 	vector<shared_ptr<Question>> questions;
 	string name;
+	bool hasPassed;
 	int countCorrectAnswers = 0;
 	double score = 0;
 	int grade = 0;
 
 public:
 
-	Test(vector<shared_ptr<Question>> questions, const string& name) :questions(questions), name(name)
+	Test(const string& name) : name(name)
 	{ }
 
 
-	void ShowAllQuestions() const
-	{
+	void ShowAllTestQuestions() {
+		string userAnswer;
+
 		for (shared_ptr<Question> object : questions)
 		{
 			object->ShowQuestion();
-			cout << endl << endl;
+			cout << "\nYour Answer: ";
+			getline(cin, userAnswer);
+
+			if (object->CheckAnswer(userAnswer)) countCorrectAnswers++;
 		}
 	}
 
@@ -242,12 +236,12 @@ public:
 	}
 
 
-	double GetScore() const
+	double GetPercentCorrectAnswers() const
 	{
-		return round(countCorrectAnswers * 10000 / questions.size()) / 100;
+		return countCorrectAnswers * 100 / questions.size();
 	}
 
-	void SetScore(double score)
+	void SetPercentCorrectAnswers()
 	{
 		while (score > 100 || score < 0)
 		{
@@ -259,14 +253,16 @@ public:
 	}
 
 
-	void CheckTest()
+	bool GetHasPassed() const
 	{
-		for (int i = 0; i < questions.size(); i++)
-		{
-			if (questions[i]->CheckAnswer()) countCorrectAnswers++;
-		}
-
+		return hasPassed;
 	}
+
+	void SetHasPassed()
+	{
+		hasPassed = (!hasPassed);
+	}
+
 
 };
 
@@ -278,7 +274,7 @@ class Category
 
 public:
 
-	Category(vector<shared_ptr<Test>> tests, const string& name) :tests(tests), name(name)
+	Category(const string& name) : name(name)
 	{ }
 
 
@@ -299,15 +295,29 @@ public:
 	}
 
 
+	shared_ptr<Test> SelectRandomTest()
+	{
+		return tests[rand() % tests.size()];
+	}
+
+
+	vector<shared_ptr<Test>> GetTests() const
+	{
+		return tests;
+	}
+
 };
 
 
 class User
 {
 	string name, surname, username, password, paternalName, address, phoneNumber;
-	vector<shared_ptr<Category>> categories;
+	vector<shared_ptr<Test>> takenExams;
 
 public:
+
+	User() :name{}, surname{}, username{}, password{}, paternalName{}, address{}, phoneNumber{}
+	{ }
 
 	User(const string& name, const string& surname, const string& username, const string& paternalName, const string& address, const string& phoneNumber)
 		:name(name), surname(surname), username(username), paternalName(paternalName), address(address), phoneNumber(phoneNumber)
@@ -370,7 +380,7 @@ public:
 		return paternalName;
 	}
 
-	void GetPaternalName(string paternalName)
+	void SetPaternalName(string paternalName)
 	{
 		while (paternalName.length() < 3 || paternalName.length() > 15)
 		{
@@ -408,7 +418,7 @@ public:
 	{
 		while (phoneNumber.length() != 10 && phoneNumber.length() != 13)
 		{
-			cout << "\nThe Phone Number Length should be between 3 and 15.\n\nNew Phone Number: ";
+			cout << "\nThe Phone Number Length should be between 10 or 13.\n\nNew Phone Number: ";
 			cin >> phoneNumber;
 		}
 
@@ -416,48 +426,590 @@ public:
 	}
 
 
-	void ShowAllCategories()
+	string GetPassword() const
 	{
-		for (shared_ptr<Category> object : categories)
+		return password;
+	}
+
+	void SetPassword(string password)
+	{
+		while (password.length() < 6 || password.length() > 15)
 		{
-			cout << object->GetCategoryName() << endl;
+			cout << "\nThe Password Length should be between 6 and 20.\n\nNew Password: ";
+			cin >> password;
+		}
+
+		this->password = password;
+	}
+
+
+	void ShowPreviousTestResults()
+	{
+		for (const auto& exams : takenExams)
+		{
+			cout << exams->GetName() << endl;
+			cout << exams->GetHasPassed() << endl;
+			cout << exams->GetCountCorrectAnswers() << endl;
+			cout << exams->GetPercentCorrectAnswers() << endl;
+			cout << exams->GetGrade() << endl << endl;
 		}
 	}
 
-	void ShowTestResult()
+
+	void NewTest(shared_ptr<Test> test)
+	{
+		test->ShowAllTestQuestions();
+		takenExams.push_back(test);
+
+		cout << "Username: " << username << endl;
+		ShowPreviousTestResults();
+	}
+
+};
+
+
+class Database
+{
+	vector<shared_ptr<Category>> categories;
+	vector<shared_ptr<User>> users;
+	shared_ptr<Admin> admin;
+
+public:
+
+	Database() :categories{}, users{}, admin{}
+	{ }
+
+
+	vector<shared_ptr<Category>>& GetCategories()
+	{
+		return categories;
+	}
+
+
+	vector< shared_ptr<User>>& GetUsers()
+	{
+		return users;
+	}
+
+
+	shared_ptr<Admin>& GetAdmin()
+	{
+		return admin;
+	}
+
+
+
+	void ShowAllUsers() const
+	{
+		for (shared_ptr<User> user : users)
+		{
+			cout << user->GetName() << endl;
+			cout << user->GetSurame() << endl;
+			cout << user->GetUsername() << endl;
+			cout << user->GetPaternalName() << endl;
+			cout << user->GetPassword() << endl;
+			cout << user->GetAddress() << endl;
+			cout << user->GetPhoneNumber() << endl;
+		}
+	}
+
+
+	void ShowAllTests()
+	{
+		for (const auto& item : categories)
+		{
+
+		}
+	}
+
+};
+
+
+class Admin
+{
+	string username, password;
+	Database& db;
+
+public:
+
+	Admin(Database& database) :username{}, password{}, db(database)
+	{ }
+
+	Admin(string username, string password, Database& database) :username(username), password(password), db(database)
+	{ }
+
+
+	string GetAdminUsername() const
+	{
+		return username;
+	}
+
+	void SetAdminUsername(string username)
+	{
+		while (username.length() < 3 || username.length() > 15)
+		{
+			cout << "Enter Straight Length Admin Username.";
+			cin >> username;
+		}
+
+		this->username = username;
+	}
+
+
+	string GetAdminPassword() const
+	{
+		return password;
+	}
+
+	void SetAdminPassword(string password)
+	{
+		while (password.length() < 6 || password.length() > 15)
+		{
+			cout << "Enter Straight Length Admin Password.";
+			cin >> password;
+		}
+
+		this->password = password;
+	}
+
+
+	void AddCategory()
+	{
+		string categoryName;
+
+		cout << "Enter Category Name: ";
+		cin >> categoryName;
+
+		shared_ptr<Category> category = make_shared<Category>(categoryName);
+
+		db.GetCategories().push_back(category);
+	}
+
+
+	void AddTest()
+	{
+		string testName;
+		int categoryInput;
+
+		for (size_t i = 0; i < db.GetCategories().size();i++)
+		{
+			cout << i + 1 << "." << db.GetCategories()[i] << endl;
+		}
+
+		cout << "\nYour Choice: ";
+		cin >> categoryInput;
+
+		cout << "Enter Test Name: ";
+		cin >> testName;
+
+		shared_ptr<Test> test = make_shared<Test>(testName);
+
+		db.GetCategories()[categoryInput - 1]->GetTests().push_back(test);
+	}
+
+
+	void addQuestion()
 	{
 
 	}
+
+};
+
+
+class UserManager
+{
+	Database& db;
+
+public:
+
+	UserManager(Database& database) :db(database)
+	{ }
+
+
+	void UserRegister(string name, string surname, string username, string password, string paternalName, string address, string phoneNumber)
+	{
+		shared_ptr<User> user = make_shared<User>();
+
+		user->SetName(name);
+		user->SetSurname(surname);
+		while (!ValidationUser(username))
+		{
+			cin >> username;
+			ValidationUser(username);
+		}
+		user->SetUsername(username);
+		user->SetPassword(password);
+		user->SetPaternalName(paternalName);
+		user->SetAddress(address);
+		user->SetPhoneNumber(phoneNumber);
+
+		db.GetUsers().push_back(user);
+
+		cout << "Register is Successful";
+	}
+
+
+	bool ValidationUser(string username)
+	{
+		for (const auto& user : db.GetUsers())
+		{
+			if (user->GetUsername() == username) return false;
+		}
+
+		return true;
+	}
+
+
+	shared_ptr<User> UserLogin(string username, string password)
+	{
+		for (const auto& user : db.GetUsers())
+		{
+			if (user->GetUsername() == username && user->GetPassword() == password)
+			{
+				cout << "Login is Successful.";
+				return user;
+			}
+		}
+
+		cout << "Invalid Password or Username";
+		return nullptr;
+	}
+
+};
+
+
+class AdminManager
+{
+	Database& db;
+
+public:
+
+	AdminManager(Database& database) :db(database)
+	{ }
+
+
+	void AdminRegister(string username, string password)
+	{
+		if (db.GetAdmin())
+		{
+			cout << "\nThere's an Admin in DB.\n";
+
+			return;
+		}
+
+		shared_ptr<Admin> admin = make_shared<Admin>(db);
+
+		admin->SetAdminUsername(username);
+		admin->SetAdminPassword(password);
+
+	}
+
+
+	shared_ptr<Admin> AdminLogin(shared_ptr<Admin> admin)
+	{
+		if (admin->GetAdminUsername() == db.GetAdmin()->GetAdminUsername() && admin->GetAdminPassword() == db.GetAdmin()->GetAdminPassword())
+		{
+			cout << "\nLogin is Successful for Admin.\n";
+			return admin;
+		}
+
+		cout << "\nInvalid Password or Username for Admin.\n";
+		return nullptr;
+	}
+
 };
 
 
 int main()
 {
 
-	srand(time(NULL));
+	//srand(time(NULL));
 
-	vector<string> options = { "10","5","1","4" };
+	//vector<string> options1 = { "10","5","1","4" };
+	//vector<string> options2 = { "4","5","6","7" };
+	//vector<string> options3 = { "4","3.5","5.5","7" };
+	//vector<string> options4 = { "f(x) = x^n","f(x) = sin(nx)","f(x) = 1/1+nx","f(x) = e^(-nx)" };
 
 
-	shared_ptr<Question> question1 = make_shared<FourOptionQuestion>(options, "Mesele: Funksiya f(x) = (x^2 - 4) / (x - 2)\nFunksiyanin x -> 2 limitini tapin.\n\n", "4");
-	shared_ptr<Question> question2 = make_shared<TrueFalseQuestion>("Mesele: Eger bir funksiya f(x) noqtesinde davamlidirsa, o zaman bu funksiyanin hemin noqtede limit deyeri de movcuddur.\n\n", "True");
-	shared_ptr<Question> question3 = make_shared<OpenEndedQuestion>("Mesele: Eger bir funksiya f(x) noqtesinde davamlidirsa, o zaman bu funksiyanin hemin noqtede limit deyeri de movcuddur.\n\n", "True");
+	//shared_ptr<Question> question1 = make_shared<FourOptionQuestion>(options1, "Funksiya f(x) = (x^2 - 4) / (x - 2)\nFunksiyanin x -> 2 limitini tapin.\n\n", "4");
+	//shared_ptr<Question> question2 = make_shared<FourOptionQuestion>(options2, "Bir funksiya f(x) = x^3 - 3x^2 + 4 noqtesinde x = 2 ucun toreme f'(x)-in deyeri nedir?\n\n", "5");
+	//shared_ptr<Question> question3 = make_shared<FourOptionQuestion>(options3, "lim_{x -> 0} (sin(x) / x) limitinin qiymeti nedir?\n\n", "3.5");
+	//shared_ptr<Question> question4 = make_shared<FourOptionQuestion>(options4, "Hansi funksiya uniform sekilde yaxinlasir?\n\n", "f(x) = 1/1+nx");
+	//shared_ptr<Question> question5 = make_shared<TrueFalseQuestion>("Bir funksiya əgər hər yerdə diferensialdırsa, o, mütləq kəsilməzdir.\n\n", "True");
+	//shared_ptr<Question> question6 = make_shared<TrueFalseQuestion>("Bir limitin mövcud olması üçün sol və sağ limitlər həmişə fərqli ola bilər.\n\n", "False");
+	//shared_ptr<Question> question7 = make_shared<TrueFalseQuestion>("Bir funksiya yalnız bir ekstremum nöqtəsinə sahibdirsə, bu nöqtə lokal minimum və ya maksimumdur.\n\n", "True");
+	//shared_ptr<Question> question8 = make_shared<OpenEndedQuestion>("lim_{x -> 2} (x^2 - 4) / (x - 2) limitinin qiymeti nedir?", "4");
+	//shared_ptr<Question> question9 = make_shared<OpenEndedQuestion>("Bir funksiya f(x) = x^2 + 3x - 5 ucun f(3)-un deyeri nedir?\n\n", "13");
+	//shared_ptr<Question> question10 = make_shared<OpenEndedQuestion>("f(x) = x^3 - 6x^2 + 9x + 1 funksiyasinin x = 2 noqtesindeki toremesinin qiymeti nedir?\n\n", "-3");
 
-	vector<shared_ptr<Question>> questions;
-	questions.push_back(question1);
-	questions.push_back(question2);
-	questions.push_back(question3);
+	//vector<shared_ptr<Question>> questions;
+	//questions.push_back(question1);
+	//questions.push_back(question2);
+	//questions.push_back(question3);
 
-	Test test1(questions, "Riyazi Analiz");
-	Test test2(questions, "Riyazi Analiz 2");
-	Test test3(questions, "Kompleks Analiz");
+	//Test test1(questions, "Riyazi Analiz");
+	//Test test2(questions, "Riyazi Analiz 2");
+	//Test test3(questions, "Kompleks Analiz");
 
-	test1.ShowAllQuestions();
-	test1.CheckTest();
+	//test1.ShowAllTestQuestions();
+	//test1.CheckTest();
 
-	cout << "CountCorrectAnswers: " << test1.GetCountCorrectAnswers() << endl;
-	cout << "Score: " << test1.GetScore() << endl;
-	cout << "Grade: " << test1.GetGrade() << endl;
 
+	Database database;
+	UserManager usermanager(database);
+
+	int userChoice, userChoice2, userChoice3, userChoice4,
+		adminChoice, adminChoice2, adminChoice3, adminChoice4, adminChoice5, adminChoice6;
+
+	while (true)
+	{
+		cout << "1.User\n2.Admin\n\nYour Choice: ";
+		cin >> userChoice;
+		system("cls");
+
+		while (true)
+		{
+			switch (userChoice)
+			{
+
+			case 1:    // User
+
+				cout << "1.Login\n2.Register\n3.Back\n\nYour Choice: ";
+
+				cin >> userChoice2;
+				system("cls");
+
+				switch (userChoice2)
+				{
+
+				case 1:	  // Login
+				{
+					while (true)
+					{
+						string usernameUserInput, passwordUserInput;
+						int	categoryChoice, testChoice;
+
+						cout << "\nEnter Username: ";
+						cin >> usernameUserInput;
+
+						cout << "\nEnter Password: ";
+						cin >> passwordUserInput;
+
+						shared_ptr<User> currentlyUser = usermanager.UserLogin(usernameUserInput, passwordUserInput);
+						if (currentlyUser)
+						{
+							system("cls");
+							cout << "1.Previous Results\n2.New Test\n\nYour Choice: ";
+							cin >> userChoice3;
+
+							switch (userChoice3)
+							{
+
+							case 1:  // Previous Results
+
+								currentlyUser->ShowPreviousTestResults();
+
+							case 2:  // New Test
+
+								for (size_t i = 0; i < database.GetCategories().size();i++)
+								{
+									cout << i + 1 << "." << database.GetCategories()[i]->GetCategoryName() << endl;
+								}
+
+								cout << "\nSelect Categories: ";
+								cin >> categoryChoice;
+
+								for (size_t i = 0;i < database.GetCategories()[categoryChoice - 1]->GetTests().size();i++)
+								{
+									cout << i + 1 << "." << database.GetCategories()[categoryChoice - 1]->GetTests()[i]->GetName() << endl;
+								}
+
+								cout << "\nSelect Test: ";
+								cin >> testChoice;
+
+								currentlyUser->NewTest(database.GetCategories()[categoryChoice - 1]->GetTests()[testChoice - 1]);
+
+							default:
+								cout << "\nChoice Correct Choice.\n";
+								break;
+							}
+						}
+
+						else
+						{
+							continue;
+						}
+
+						break;
+					}
+				}
+
+				case 2:	  // Register
+				{
+					string nameUserInput, surnameUserInput, usernameUserInput, passwordUserInput, paternalUserInput, addressUserInput, phoneNumberUserInput;
+					system("cls");
+					cout << "\nEnter Name: ";
+					cin >> nameUserInput;
+
+					cout << "\nEnter Surname: ";
+					cin >> surnameUserInput;
+
+					cout << "\nEnter Username: ";
+					cin >> usernameUserInput;
+
+					cout << "\nEnter Password: ";
+					cin >> passwordUserInput;
+
+					cout << "\nEnter Paternal Name: ";
+					cin >> paternalUserInput;
+
+					cout << "\nEnter Address: ";
+					cin >> addressUserInput;
+
+					cout << "\nEnter Phone Number: ";
+					cin >> phoneNumberUserInput;
+
+					usermanager.UserRegister(nameUserInput, surnameUserInput, usernameUserInput, passwordUserInput, paternalUserInput, addressUserInput, phoneNumberUserInput);
+					system("cls");
+
+					break;
+				}
+
+				case 3:	   // Back
+				{
+					break;
+				}
+
+				default:
+					cout << "\nChoice Correct Choice.\n";
+					break;
+				}
+
+			case 2:    // Admin
+
+				cout << "";
+
+				switch (adminChoice)
+				{
+
+				case 1:	   // User Management
+
+					switch (adminChoice2)
+					{
+
+					case 1:	   // Create User	 
+
+
+
+					case 2:	   // Update User
+
+
+
+					case 3:	   // Delete User
+
+
+
+					default:
+						cout << "\nChoice Correct Choice.\n";
+						break;
+					}
+
+				case 2:	   // Statistics
+
+					switch (adminChoice3)
+					{
+
+					case 1:	   // All Test Results 
+
+
+
+					case 2:	   // All Category Test Results
+
+
+
+					case 3:	   // Test Result
+
+
+
+					case 4:	   // User Results	
+
+
+
+					default:
+						cout << "\nChoice Correct Choice.\n";
+						break;
+					}
+
+				case 3:	   // Test Management
+
+					switch (adminChoice4)
+					{
+
+					case 1:	   // Add Category
+
+
+
+					case 2:	   // Add Test
+
+
+
+					case 3:	   // Add Question
+
+
+
+					case 4:	   // Category Export/Import
+
+						switch (adminChoice5)
+						{
+
+						case 1:	  // Category Export
+
+
+
+						case 2:	  // Category Import
+
+
+
+						default:
+							break;
+						}
+
+					case 5:	   // Test Export/Import
+
+						switch (adminChoice6)
+						{
+
+						case 1:	  // Test Export
+
+
+
+						case 2:	  // Test Import
+
+
+
+						default:
+							break;
+						}
+
+					default:
+						break;
+					}
+
+				default:
+					break;
+				}
+
+
+
+			default:
+				break;
+
+
+				break;
+			}
+		}
+
+
+	}
 
 }

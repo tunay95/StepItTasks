a23be673eed4b12c2f25f84a5e878b52431b54b2
tunay95 <cypherhsynl1@gmail.com>
2025-02-24 02:01:12 +0400

    Insert & Select Data Task(SqlConnection, SqlCommand, SqlDataReader) was finished.

---
+++ Ado.NET-Tasks/Ado.NET Task1/Ado.NET Task1/User.cs
@@ -0,0 +1,28 @@
+﻿
+/*
+ Console üçün Register & Login proqramı yazın. Bütün məlumatlar Database-e yazilmali və oxunmalıdır
+1. Sign In
+2. Sign Up
+3. Exit
+*/
+
+
+public class User 
+{
+    public static int Id = 1;
+
+	public User(string firstName, string lastName, int age, string login, string password)
+	{
+		FirstName = firstName;
+		LastName = lastName;
+		Age = age;
+		Login = login;
+		Password = password;
+	}
+
+	public string FirstName { get; set; }    
+    public string LastName { get; set; }    
+    public int Age { get; set; }
+	public string Login { get; set; }
+	public string Password { get; set; }
+}
